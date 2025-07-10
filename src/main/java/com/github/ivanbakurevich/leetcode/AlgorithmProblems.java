067f3b213fc61e4afbc46d66854a9d7d2ab57326
Ivan Bakurevich <bakurevich.ivan@gmail.com>
2025-07-10 17:29:40 +0300

    Problems

--- src/main/java/com/github/ivanbakurevich/leetcode/AlgorithmProblems.java
+++ src/main/java/com/github/ivanbakurevich/leetcode/AlgorithmProblems.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class AlgorithmProblems {
 
@@ -25,6 +26,7 @@ public class AlgorithmProblems {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+
 
     public static class ListNode {
         int val;
@@ -75,5 +77,99 @@ public class AlgorithmProblems {
         return result;
     }
 
+
+    /**
+     * Solution to <a href="https://leetcode.com/problems/merge-strings-alternately">...</a>
+     */
+    public static String mergeAlternately(String word1, String word2) {
+        var builder = new StringBuilder();
+        for (var i = 0; i < word1.length() || i < word2.length(); i++) {
+            if (i < word1.length()) {
+                builder.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                builder.append(word2.charAt(i));
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Solution to <a href="https://leetcode.com/problems/find-pivot-index">...</a>
+     */
+    public static int pivotIndex(int[] nums) {
+        long sum = 0L;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        long rightSum = sum;
+        long leftSum = 0L;
+
+        for(int i=0;i<nums.length;i++) {
+            rightSum -= nums[i];
+            if(leftSum == rightSum) {
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+
+        return -1;
+    }
+
+    /**
+     * Solution to <a href="https://leetcode.com/problems/longest-common-prefix/">...</a>
+     */
+    public static String longestCommonPrefix(String[] strs) {
+        String currentString = strs[0];
+        for(int i=1;i<strs.length;i++) {
+            currentString = findCommon(currentString, strs[i]);
+            if(currentString.isEmpty()){
+                break;
+            }
+        }
+        return currentString;
+    }
+
+    private static String findCommon(String str1, String str2) {
+        int minLength = Math.min(str1.length(), str2.length());
+        int index = 0;
+
+        while (index < minLength && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+
+        return str1.substring(0, index);
+    }
+
+    /**
+     * Solution to <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">...</a>
+     * TODO
+     */
+//    public int lengthOfLongestSubstring(String s) {
+//        var existing = new HashMap<Character, Integer>();
+//        int maxSubstringLength = -1;
+//
+//        for (int i = 0; i <= s.length(); i++) {
+//            Integer prevIndex = existing.put(s.charAt(i), i);
+//            if (prevIndex != null) {
+//                maxSubstringLength = Math.max(maxSubstringLength, i - prevIndex);
+//            }
+//
+//            if( i == s.length()) {
+//                existing.keySet()
+//                        .forEach(k -> {
+//                            existing.put()
+//                        });
+//            }
+//        }
+//
+//        Integer minIndex = existing.values().stream()
+//                .min(Integer::compare)
+//                .get();
+//
+//        maxSubstringLength = Math.max(maxSubstringLength, s.length() - minIndex);
+//
+//        return maxSubstringLength == -1 ? s.length() : maxSubstringLength;
+//    }
 
 }
